9e62b10d5b7784c78068fa458788e5a1ec6d0f41
Mohebullah Toofan <mohet3@gmail.com>
2019-03-01 22:43:09 +0100

    App remembers if user has logged in and skips log in screen if the user is logged in.

--- app/src/main/java/com/example/firebasechat/Activities/LogInActivity.java
+++ app/src/main/java/com/example/firebasechat/Activities/LogInActivity.java
@@ -19,31 +19,58 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LogInActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
         private SignInButton GoogleSignIn;
         private LoginButton facebookButton;
-        private GoogleApiClient googleApiClient;
-        private static final int REQ_CODE = 9001;
+        private static int RC_SIGN_IN = 9001;
         private CallbackManager callbackManager;
+        AuthStateListener mAuthListener;
+        FirebaseAuth mAuth;
+        private GoogleApiClient mGoogleApiClient;
+        private GoogleSignInClient mGoogleSignInClient;
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         callbackManager = CallbackManager.Factory.create();
         facebookButton = (LoginButton)findViewById(R.id.facebookButton);
         GoogleSignIn = (SignInButton)findViewById(R.id.google_loginBtn);
         GoogleSignIn.setOnClickListener(this);
-        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("91228041707-fbnlqhef5vsa6dsh3fqc0ufkmfb6ogub.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
         final Intent intent = new Intent(this,ChatRoomList.class);
 
         //handling results from facebook login
@@ -52,6 +79,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             public void onSuccess(LoginResult loginResult) {
                 //show successfull log in message and switch to chat room list activity
                 Toast.makeText(LogInActivity.this,"Succesfully logged in",Toast.LENGTH_SHORT).show();
+                handleFacebookAccessToken(loginResult.getAccessToken());
                 startActivity(intent);
             }
 
@@ -66,80 +94,108 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
             }
         });
 
+        mAuthListener = new AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null){
+                    Intent intent = new Intent(LogInActivity.this,ChatRoomList.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        if (AccessToken.getCurrentAccessToken()!=null){
+            startActivity(intent);
+        }
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(LogInActivity.this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(true);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                           ;
+                            //updateUI(false);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
     //Handling clicks on login buttons
     @Override
     public void onClick(View v) {
-
         switch (v.getId())
         {
             case R.id.google_loginBtn:
-                googleSignIn();
-                break;
-
+                signIn();
         }
-
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
     //Opening Google Log In activity
-    public void googleSignIn()
-    {
-        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(intent,REQ_CODE);
-
+    private void signIn() {
+        System.out.println("hejhejehej");
+        Intent signIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signIntent,RC_SIGN_IN);
     }
 
-    // Handling results of Google Log In Activity
-    public void handleGoogleSignInResult(GoogleSignInResult result)
-    {
-        if(result.isSuccess())
-        {
-            updateUI(true);
-        }
-        else
-        {
-            Toast.makeText(LogInActivity.this,"An error occurred, try again",Toast.LENGTH_SHORT).show();
-        }
-
-    }
-    //Move to chatroom activity when sign in successful
-    public void updateUI(boolean isLoggedIn)
-    {
-        Intent intent = new Intent(this,ChatRoomList.class);
-        if(isLoggedIn)
-        {
-            startActivity(intent);
-        }
-    }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == REQ_CODE)
-        {
+        if(requestCode==RC_SIGN_IN){
+            System.out.println("test2");
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleGoogleSignInResult(result);
+            if(result.isSuccess()){
+                System.out.println("test1");
+                GoogleSignInAccount account = result.getSignInAccount();
+                authWithGoogle(account);
+            }
         }
-
         callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 
-    //Getting Users name from facebook login
-    AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
-        @Override
-        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+    private void authWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    System.out.println("hawewae");
+                    startActivity(new Intent(getApplicationContext(),ChatRoomList.class));
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Auth Error",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
-        }
-    };
     //Getting Users name from facebook
     private void getFacebookUsername (AccessToken newAccessToken)
     {
 
     }
+
+
 }
